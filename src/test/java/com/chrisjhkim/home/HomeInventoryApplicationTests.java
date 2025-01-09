41ae8b453d9054e3eefa07d69e57a4afdc8eec3b
chrisjhkim <chris1108g@gmail.com>
2025-01-09 23:45:19 +0900

    initial commit

---
+++ src/test/java/com/chrisjhkim/home/HomeInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisjhkim.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
